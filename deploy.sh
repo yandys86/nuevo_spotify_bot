7effe2980d6428cfba2f9b4bdd65f9fc65a5512c
yandys86 <70566671+yandys86@users.noreply.github.com>
2026-05-18 00:21:42 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@
 set -u
 
 # Lista de VM_IDs a actualizar (separados por espacio)
-VMS="121,122,123,124,125"
+VMS="103 105 106 107 108 109 110 111 112 113 114 115 116 117 118 119 120 121 122 123 124 125 126 127 128 129 130"
 
 # Verificar que deploy_one.sh existe y es ejecutable
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
